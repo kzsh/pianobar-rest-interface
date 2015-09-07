8dd37e7af5e154721b71d85c2ece6d98d4c70c14
Andrew Hunt <andrew@kzsh.local>
2015-09-07 10:31:24 -0400

    Cleaning up station/:id endpoint and supporting lib

--- routes/stations.js
+++ routes/stations.js
@@ -1,36 +1,17 @@
 var router = require('express').Router();
 var sendMessage = require('../lib/PianoLib').sendMessage;
 
-function cleanArray(actual){
-  var newArray = [];
-  for(var i = 0; i<actual.length; i++){
-      if (actual[i]){
-        newArray.push(actual[i]);
-    }
-  }
-  return newArray;
-}
 // ===================================================
 // ===================================================
 
-router.get('/', function (req, res, next) {
-  sendMessage("s").then(function (data) {
-      var dataList = data.split("\n");
-      dataList = dataList.slice(1,dataList.length);
-      var list = dataList.map(function (item) {
-        var result = item.match(/q/);
-        console.log(result);
-        return item;
-      });
-    list = cleanArray(list)
-    console.log(list)
-    res.send(list);
-  });
-});
 
 router.post('/:id', function(req, res, next) {
   var stationMessage = parseInt(req.params.id, 10)
-  return sendMessage("s" + stationMessage);
+  sendMessage("s" + stationMessage).then(function () {
+    res.sendStatus("OK");
+  }).catch(function (error) {
+      res.send(error);
+  });
 });
 
 // internal functions
