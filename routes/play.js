e40592d3f99e07470b779a100a33f218c5ec53ec
Andrew <andrew@hunt.li>
2016-09-06 13:40:57 -0400

    Fix status reporting

--- routes/play.js
+++ routes/play.js
@@ -2,10 +2,10 @@ var router = require('express').Router();
 var sendMessage = require('../lib/PianoLib').sendMessage;
 
 router.post('/', function(req, res, next) {
-  sendMessage("p").then(function () {
-    res.sendStatus("OK");
+  sendMessage("p").then(function (data) {
+    res.sendStatus(200);
   }).catch(function (error) {
-      res.send(error);
+    res.sendStatus(500);
   });
 });
 
--- routes/volume.js
+++ routes/volume.js
@@ -6,25 +6,28 @@ var sendMessage = require('../lib/PianoLib').sendMessage;
 
 router.post('/up', function(req, res, next) {
   sendMessage(")").then(function (resolve, reject) {
-    res.sendStatus("OK");
+    res.sendStatus("200");
   }).catch(function (error) {
-      res.send(error);
+    console.error(error);
+    res.sendStatus("500");
   });
 });
 
 router.post('/down', function(req, res, next) {
   sendMessage("(").then(function (resolve, reject) {
-    res.sendStatus("OK");
+    res.sendStatus("200");
   }).catch(function (error) {
-      res.send(error);
+    console.error(error);
+    res.sendStatus("500");
   });
 });
 
 router.post('/reset', function(req, res, next) {
   sendMessage("0").then(function (resolve, reject) {
-    res.sendStatus("OK");
+    res.sendStatus("200");
   }).catch(function (error) {
-      res.send(error);
+    console.error(error);
+    res.sendStatus("500");
   });
 });
 
