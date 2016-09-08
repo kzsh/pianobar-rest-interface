4f4776fd3c23859599cd8d01b3ff7f044f5964a1
Andrew <andrew@hunt.li>
2016-09-07 21:15:18 -0400

    provide stations at /stations endpoint and extract parser to lib

--- routes/stations.js
+++ routes/stations.js
@@ -1,30 +1,19 @@
 var router = require('express').Router();
+
+var PianoOutoutParser = require('../lib/PianoOutputParser');
+
 var sendMessage = require('../lib/PianoLib').sendMessage;
 
-function parseData(string) {
-  const lines = string.replace(/\0/g,'').replace(/\u001b\[2K(\t|\[\?\])/g, '').split('\n');
-  const newStationLine = lines[lines.length - 2];
-  const newStationId = Number(newStationLine.match(/\d+/)[0]);
+const { getSelectedStation, getStations } = PianoOutoutParser;
 
-  let newStationName;
-  lines.forEach(function(line) {
-    if (newStationName) return;
 
-    const stationId = Number(line.match(/\d+/)[0]);
-    if (stationId === newStationId) {
-      newStationName = line.replace(/\d{1,2}\)\s*q\s*/, '');
-    }
-  });
-
-  return {
-    id: newStationId,
-    name: newStationName
-  };
-}
 
 router.get('/', function(req, res, next) {
-  sendMessage("s\n").then(function (data) {
-    res.sendStatus(200);
+  sendMessage("s").then(function (data) {
+    var json = getStations(data);
+    res.set('Content-Type', 'application/json');
+    res.send(JSON.stringify({ stations: json }));
+    sendMessage("\n")
   }).catch(function (error) {
     console.error(error);
     res.sendStatus(500);
@@ -34,7 +23,8 @@ router.get('/', function(req, res, next) {
 router.post('/:id', function(req, res, next) {
   var stationMessage = parseInt(req.params.id, 10)
   sendMessage("s" + stationMessage).then(function (data) {
-    var json = parseData(data);
+    var json = getSelectedStation(data);
+    res.set('Content-Type', 'application/json');
     res.send(JSON.stringify(json));
   }).catch(function (error) {
     console.error(error);
